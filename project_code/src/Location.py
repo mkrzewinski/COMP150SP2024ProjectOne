406d26ace6542948c11ed0006c96bb402f68030c
Milosz M Krzewinski <143234477+mkrzewinski@users.noreply.github.com>
2024-04-18 10:11:22 -0500

    Commit 04.18

--- project_code/src/Location.py
+++ project_code/src/Location.py
@@ -1,2 +1,60 @@
+
+def create_parser():
+    return UserInputParser()
+    parser = create_parser()
+    parser = UserInputParser()
+
 class Location:
-    pass
+
+# List of location names
+    location_names = [
+    "Temple of the Sheep",
+    "Baaaadmans Village",
+    "Sheep Desert",
+    "Tomb of Lambs",
+    "Ewe-ok Village",
+    "The Lost Valley",
+    "The Palace of Dolly",
+    "Enchanted Woods",
+    "Mary's Dreamy Wonderland",
+    "City of Mutton"
+]
+
+
+import random
+
+def __init__(self, name, parser, number_of_events: int = 1):
+        self.name = name
+        self.parser = parser
+        self.events = [Event(self.parser) for _ in range(number_of_events)]
+
+def create_custom_event_from_static_text_file(self, file_path):
+
+        return Event(self.parser, data)
+
+# List of location names
+location_names = [
+    "Temple of the Sheep",
+    "Baaaadmans Village",
+    "Sheep Desert",
+    "Tomb of Lambs",
+    "Ewe-ok Village",
+    "The Lost Valley",
+    "The Palace of Dolly",
+    "Enchanted Woods",
+    "Mary's Dreamy Wonderland",
+    "City of Mutton"
+]
+
+import random
+
+# Assuming you have a function create_parser() that creates the parser
+parser = create_parser()
+
+# Assuming location_names is already defined
+locations = [Location(name, parser) for name in random.sample(location_names, 10)]
+
+def __init__(self, parser, number_of_events: int = 1):
+        self.parser = parser
+        self.events = [Event(self.parser) for _ in range(number_of_events)]
+pass
