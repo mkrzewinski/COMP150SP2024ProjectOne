29057ea3cbdd48fd1a23aa572b25842c7a9d3381
Milosz M Krzewinski <143234477+mkrzewinski@users.noreply.github.com>
2024-04-18 10:10:28 -0500

    Commit 04.18

--- project_code/src/Event.py
+++ project_code/src/Event.py
@@ -1,2 +1,31 @@
+
+
+from enum import Enum 
+
+class EventStatus(Enum):
+
+
+
 class Event:
+    def __init__(self, parser):
+        self.parser = parser 
+        self.fail = {
+            ""
+        }
+        self.pass_ = {
+            ""
+        }
+        self.partial_pass = {
+            ""
+        }
+    self.prompt_text = "prompt"
+
+    #self.primary: Stat
     pass
+
+def execute(self, party):
+    chosen_one = self.parser.select_party_member(party)
+    chosen_skill = self.parser.select_skill(chosen_one)
+
+    self.set_status(EventStatus.PASS)
+    pass 
